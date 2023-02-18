3c1f2ce13b6470b2e7a367cf016a2f10a1d58031
linhngx <linhngx9@gmail.com>
2023-02-18 21:52:42 +0100

    second

--- littlelemon/src/App.js
+++ littlelemon/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import BookingPage from './pages/BookingPage';
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/booking">Book a Table</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/booking" element={<BookingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ littlelemon/src/pages/BookingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BookingPage() {
+  return (
+    <div>
+      <h1>BookingPage</h1>
+    </div>
+  );
+}
+
+export default BookingPage;
